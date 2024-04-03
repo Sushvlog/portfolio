b5bdfc22a3111c6ed33a79c50020799a48ea31f0
Sushvlog <sushmitagaikwad805@gmail.com>
2024-04-04 01:56:23 +0530

    first commit

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // Add other PostCSS plugins here if needed
+  }
+};
